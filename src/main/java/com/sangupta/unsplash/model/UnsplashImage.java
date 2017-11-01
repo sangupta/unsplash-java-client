5cc52af68982bea3547d550ff4ec959bc4092947
Sandeep Gupta <sangupta@adobe.com>
2017-11-01 11:48:36 -0700

    Added hashcode/equals/tostring methods

--- src/main/java/com/sangupta/unsplash/model/UnsplashImage.java
+++ src/main/java/com/sangupta/unsplash/model/UnsplashImage.java
@@ -54,5 +54,54 @@ public class UnsplashImage {
 	public UnsplashExif exif;
 	
 	public UnsplashLocation location;
+	
+	public String getImageUrl() {
+		if(this.urls == null) {
+			return null;
+		}
+		
+		return this.urls.raw;
+	}
 
+	@Override
+	public int hashCode() {
+		if(this.id == null) {
+			return 0;
+		}
+		
+		return this.id.hashCode();
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if(obj == null) {
+			return false;
+		}
+		
+		if(this == obj) {
+			return true;
+		}
+		
+		if(this.id == null) {
+			return false;
+		}
+		
+		if(obj instanceof UnsplashImage) {
+			UnsplashImage other = (UnsplashImage) obj;
+			
+			return this.id.equals(other.id);
+		}
+		
+		if(obj instanceof String) {
+			return this.id.equals(obj); 
+		}
+		
+		return false;
+	}
+
+	@Override
+	public String toString() {
+		return "UnsplashImage: " + this.getImageUrl();
+	}
+	
 }
